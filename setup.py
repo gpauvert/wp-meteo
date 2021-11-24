8b404c716cc0f49dc835e1f118d29d31ec44aa74
gpauvert <g.pauvert@hotmail.com>
2021-11-24 15:38:27 +0200

    Added additional parameters

--- setup.py
+++ setup.py
@@ -1,20 +1,14 @@
-from setuptools import setup
-
-
-with open('README.md') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    lic = f.read()
+from distutils.core import setup
 
 setup(
     name="wp-meteo",
     version="0.1.0",
-    long_description=readme,
+    description="Python package to easily load exported WindPRO meteo text files into a pandas DataFrame.",
     url="https://github.com/gpauvert/wp-meteo",
     author="Guillaume Pauvert",
     author_email="g.pauvert@hotmail.com",
-    license=lic,
+    keywords=["wind", "windpro"],
+    license="gpl-3.0",
     packages=["wp_meteo"],
     install_requires=["pandas"]
 )
